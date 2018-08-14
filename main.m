849cafe54aecf3f293c70c9a73324443892128b8
errard70101 <errard70101@gmail.com>
2018-08-14 10:53:39 +0800

    Update
    1. Add progress bar.
    2. Add drop importer each year setting.

--- main.m
+++ main.m
@@ -1,5 +1,6 @@
 % Load setup file
 
+% Please modify the line below to set the correct path
 path = "C:/ppml_simulation/";
 setup_file_name = "setup.csv";
 disp('Loading setup files...')
@@ -8,10 +9,19 @@ setup = readtable(strcat(path, setup_file_name));
 disp('Finish loading.')
 toc
 
-n_setup = zeros(2, 1);
-mean_error = cell(2, 1);
-RMSE = cell(2, 1);
-t = table(n_setup, mean_error, RMSE);
+n_setup = zeros(height(setup), 1);
+mean_error = cell(height(setup), 1);
+RMSE = cell(height(setup), 1);
+elapsed_time = zeros(height(setup), 1);
+n_simulation = zeros(height(setup), 1);
+n_countries = zeros(height(setup), 1);
+n_year = zeros(height(setup), 1);
+true_params = cell(height(setup), 1);
+drop_importer_each_year = zeros(height(setup), 1);
+t = table(n_setup, mean_error, RMSE, elapsed_time, n_simulation, ...
+    n_countries, n_year, true_params, drop_importer_each_year);
+
+clear n_setup mean_error RMSE elapsed_time n_simulation n_countries n_year true_params drop_importer_each_year
 
 stop_time = zeros(size(setup, 1), 1);
 for s = 1:size(setup, 1)
@@ -22,16 +32,26 @@ n_simulation = setup.n_simulation(s);
 n_countries = setup.n_countries(s);
 n_year = setup.n_year(s);
 b = str2array(setup.b(s))';
+drop_importer_each_year = setup.drop_importer_each_year(s);
 n_obs = (n_countries^2)*n_year;
 
 
 % Generate simuation data according to setup file
+disp('Generating the MR terms...')
 mr_term = create_bilateral_year_mr_term(n_countries, n_year);
+disp('The MR terms are generated.')
 
-% Drop one importer fixed effect for every year
-mr_term = mr_term(:, n_countries*n_year + 1: n_countries*n_year + n_countries);
+if drop_importer_each_year == 1
+    % Drop importer fixed effects for every year
+    mr_term(:, n_countries*n_year + 1: n_countries*n_year + n_year) = [];
+else
+    % Drop one importer fixed effect
+    mr_term(:, n_countries*n_year + 1) = [];
+end
 
 b1 = zeros(length(b) + size(mr_term, 2), n_simulation);
+
+progressbar(['Running simulation, setup ', num2str(s), '.'])
 for n_sim = 1:n_simulation
     
     mr_term_coeff = randn(size(mr_term, 2), 1);
@@ -43,6 +63,7 @@ for n_sim = 1:n_simulation
 
 % Run simulation
     b1(:, n_sim) = glmfit(X, y,'poisson', 'constant', 'off');
+    progressbar(n_sim/n_simulation)
 end
 stop_time(s) = toc;
 
@@ -65,7 +86,8 @@ t.elapsed_time(s) = stop_time(s);
 t.n_simulation(s) = n_simulation;
 t.n_countries(s) = n_countries;
 t.n_year(s) = n_year;
-t.true_parms(s) = b;
+t.true_params(s) = {b};
+t.drop_importer_each_year(s) = drop_importer_each_year;
 
 end
 writetable(t, strcat(path, 'simulation-results.csv'))
